a8fa7fe68483df64dda8caad90287db7d037b363
Artur Ribeiro <profissional.arturrc@gmail.com>
2021-03-06 18:40:16 -0300

    Commit template montado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
+import Home from './pages/Home';
+
 function App() {
   return (
     <div className="App">
-      <h1>Teste</h1>
+      <Home />
     </div>
   );
 }
 
 export default App;
-
-
-code ~/.gitconfig
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Eu sou Um Header</h1>
+    </div>
+  );
+};
+
+export default Header;
